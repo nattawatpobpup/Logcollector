85a36cff57915ec15a72baf3a03814cca91af3f6
Nattawat Burirat <45628206+nattawatpobpup@users.noreply.github.com>
2020-01-29 17:24:31 +0700

    Update app.routes.ts

--- elasticUI/src/app/app.routes.ts
+++ elasticUI/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { QueryComponent } from './query/query.component';
-
+// tslint:disable-next-line:import-spacing
 const routes: Routes = [
   { path: '',  redirectTo: '/query', pathMatch: 'full' },
   {path: 'query', component : QueryComponent}
-]
+  ]
 export const routing = RouterModule.forRoot(routes);
